25de61de803798c53057f996bfb257d3ff6dae1e
Dan <dmurray186@gmail.com>
2022-08-13 13:21:56 +1000

    Fixed indent issue

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ const userSchema = new mongoose.Schema ({
 
 
 userSchema.plugin(encrypt, {
-		secret: process.env.SECRET,
-		encryptedFields: ['password']
-	});
+	secret: process.env.SECRET,
+	encryptedFields: ['password']
+});
 
 const User = new mongoose.model("User", userSchema);
 
